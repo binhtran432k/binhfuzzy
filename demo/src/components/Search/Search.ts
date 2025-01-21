0a95f20774266bc595c43f9e9f17c2a850029a61
Binh Tran <binhtran432k@gmail.com>
2025-01-21 11:53:56 +0700

    feat: enhance positions matching

--- demo/src/components/Search/Search.ts
+++ demo/src/components/Search/Search.ts
@@ -1,9 +1,12 @@
+import { computeScore, computeScoreWithPositions, hasMatch } from "binhfuzzy";
 import { env } from "mini-van-plate/shared";
 import type { Van } from "vanjs-core";
-import { hasMatch, computeScore } from "binhfuzzy";
+
+const PositionArray = Uint16Array;
+type PositionArray = typeof PositionArray.prototype;
 
 const Search = () => {
-	const { div, input, ul, li } = env.van.tags;
+	const { div, input, ul, li, b } = env.van.tags;
 
 	const searchText = env.van.state("");
 	const searchTime = env.van.state(0);
@@ -17,19 +20,48 @@ const Search = () => {
 		}, 300);
 	};
 
-	const matchedItems = env.van.derive<[value: string, score: number][]>(() => {
-		if (searchData.val) {
-			const t1 = performance.now();
-			const matches = searchData.val
-				.filter((x) => hasMatch(searchText.val, x))
-				.map((x) => [x, computeScore(searchText.val, x)] as [string, number])
-				.sort(([, a], [, b]) => b - a);
-			const t2 = performance.now();
-			searchTime.val = t2 - t1;
-			return matches;
-		}
-		return [];
-	});
+	const matchedItems = env.van.derive<[splits: string[], score: number][]>(
+		() => {
+			if (searchData.val) {
+				const t1 = performance.now();
+				const allMatches = searchData.val
+					.filter((x) => hasMatch(searchText.val, x))
+					.map((haystack) => {
+						return [haystack, computeScore(searchText.val, haystack)] as [
+							string,
+							number,
+						];
+					})
+					.sort(([, a], [, b]) => b - a);
+				const matches = allMatches
+					.slice(0, 1e3)
+					.map(([haystack]) => {
+						const positions = new PositionArray(searchText.val.length);
+						const score = computeScoreWithPositions(
+							searchText.val,
+							haystack,
+							positions,
+						);
+						return [splitByPositions(haystack, positions), score] as [
+							string[],
+							number,
+						];
+					})
+					.concat(
+						allMatches
+							.slice(1e3)
+							.map(
+								([haystack, score]) =>
+									[[haystack], score] as [string[], number],
+							),
+					);
+				const t2 = performance.now();
+				searchTime.val = t2 - t1;
+				return matches;
+			}
+			return [];
+		},
+	);
 
 	env.van.derive(async () => {
 		if (typeof window !== "undefined") {
@@ -49,7 +81,17 @@ const Search = () => {
 		}),
 		div(searchText),
 		() => div(`Matched ${matchedItems.val.length} in ${searchTime.val}ms`),
-		() => ul(matchedItems.val.slice(0, 1e3).map(([x, c]) => li(`${c} - ${x}`))),
+		() =>
+			ul(
+				matchedItems.val
+					.slice(0, 1e3)
+					.map(([splits, c]) =>
+						li(
+							`${c} - `,
+							...splits.map((x, i) => (i % 2 === 1 ? b(x) : x)).filter(Boolean),
+						),
+					),
+			),
 	);
 };
 
@@ -57,6 +99,29 @@ export function hydrateSearch(van: Van) {
 	for (const el of Array.from(document.querySelectorAll(".search"))) {
 		van.hydrate(el, (_dom) => Search());
 	}
+}
+
+function splitByPositions(
+	haystack: string,
+	positions: PositionArray,
+): string[] {
+	if (positions.length === 0 || positions[0] === 0) return [haystack];
+	const splitMatches: string[] = [haystack.slice(0, positions[0] - 1)];
+	let lastPos = positions[0];
+	for (let i = 1; i < positions.length; i++) {
+		if (positions[i] > positions[i - 1] + 1) {
+			splitMatches.push(haystack.slice(lastPos - 1, positions[i - 1]));
+			splitMatches.push(haystack.slice(positions[i - 1], positions[i] - 1));
+			lastPos = positions[i];
+		}
+	}
+	if (positions.length > 0) {
+		splitMatches.push(
+			haystack.slice(lastPos - 1, positions[positions.length - 1]),
+		);
+		splitMatches.push(haystack.slice(positions[positions.length - 1]));
+	}
+	return splitMatches;
 }
 
 export default Search;
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const SCORE_MATCH_CAPITAL = 70;
 const SCORE_MATCH_DOT = 60;
 
 const OUT_OF_SCORE_SIZE = 1024;
-const SCORE_BASE = 512;
+const SCORE_BASE = 512 + 1;
 
 const ScoreArray = Uint16Array;
 const BonusArray = Uint8Array;
@@ -21,7 +21,7 @@ type BonusArray = typeof BonusArray.prototype;
 export function hasMatch(needle: string, haystack: string): boolean {
 	const lowerNeedle = needle.toLowerCase();
 	const lowerHaystack = haystack.toLowerCase();
-	for (let i = 0, j = 0; i < lowerNeedle.length; i += 1) {
+	for (let i = 0, j = 0; i < lowerNeedle.length; i++) {
 		j = lowerHaystack.indexOf(lowerNeedle[i], j) + 1;
 		if (j === 0) return false;
 	}
@@ -48,7 +48,117 @@ export function computeScore(needle: string, haystack: string): number {
 		return SCORE_MIN;
 	}
 
-	return _computeScore(n, m, needle, haystack);
+	const normalizedNeedle = needle.toLowerCase();
+	const normalizedHaystack = haystack.toLowerCase();
+
+	const matchBonuses = precomputeBonuses(haystack);
+
+	const bestScores = new ScoreArray(m);
+	const bestMatchScores = new ScoreArray(m);
+
+	for (let i = 0; i < n; i++) {
+		computeRow(
+			i,
+			n,
+			m,
+			normalizedNeedle,
+			normalizedHaystack,
+			matchBonuses,
+			bestScores,
+			bestMatchScores,
+			bestScores,
+			bestMatchScores,
+		);
+	}
+
+	return bestScores[m - 1];
+}
+
+export function computeScoreWithPositions(
+	needle: string,
+	haystack: string,
+	positions: { [x: number]: number },
+): number {
+	const n = needle.length;
+	const m = haystack.length;
+
+	if (!n || !m) return SCORE_MIN;
+
+	if (n === m) {
+		for (let i = 0; i < n; i++) {
+			positions[i] = i + 1;
+		}
+		return SCORE_MAX;
+	}
+
+	if (m > OUT_OF_SCORE_SIZE) {
+		return SCORE_MIN;
+	}
+
+	const normalizedNeedle = needle.toLowerCase();
+	const normalizedHaystack = haystack.toLowerCase();
+
+	const matchBonuses = precomputeBonuses(haystack);
+
+	const bestScoreMatrix = createScoreMatrix(n, m);
+	const bestMatchScoreMatrix = createScoreMatrix(n, m);
+
+	computeRow(
+		0,
+		n,
+		m,
+		normalizedNeedle,
+		normalizedHaystack,
+		matchBonuses,
+		bestScoreMatrix[0],
+		bestMatchScoreMatrix[0],
+		bestScoreMatrix[0],
+		bestMatchScoreMatrix[0],
+	);
+	for (let i = 1; i < n; i++) {
+		computeRow(
+			i,
+			n,
+			m,
+			normalizedNeedle,
+			normalizedHaystack,
+			matchBonuses,
+			bestScoreMatrix[i],
+			bestMatchScoreMatrix[i],
+			bestScoreMatrix[i - 1],
+			bestMatchScoreMatrix[i - 1],
+		);
+	}
+
+	let isMatchRequired = false;
+	for (let i = n - 1, j = m - 1; i >= 0; i--) {
+		for (; j >= 0; j--) {
+			// There may be multiple paths which result in
+			// the optimal weight.
+			//
+			// For simplicity, we will pick the first one
+			// we encounter, the latest in the candidate
+			// string.
+			if (
+				bestMatchScoreMatrix[i][j] > SCORE_MIN &&
+				(isMatchRequired ||
+					bestMatchScoreMatrix[i][j] === bestScoreMatrix[i][j])
+			) {
+				// If this score was determined using
+				// SCORE_MATCH_CONSECUTIVE, the
+				// previous character MUST be a match
+				isMatchRequired =
+					i > 0 &&
+					j > 0 &&
+					bestScoreMatrix[i][j] ===
+						bestMatchScoreMatrix[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE;
+				positions[i] = 1 + j--;
+				break;
+			}
+		}
+	}
+
+	return bestScoreMatrix[n - 1][m - 1];
 }
 
 function precomputeBonuses(haystack: string): BonusArray {
@@ -56,10 +166,8 @@ function precomputeBonuses(haystack: string): BonusArray {
 	const matchBonuses = new BonusArray(haystack.length);
 
 	let lastChar = "/";
-	let lastIsUpper = true;
 	for (let i = 0; i < haystack.length; i++) {
 		const ch = haystack[i];
-		const isUpper = ch.toUpperCase() === ch;
 
 		switch (lastChar) {
 			case "/":
@@ -74,7 +182,8 @@ function precomputeBonuses(haystack: string): BonusArray {
 				matchBonuses[i] = SCORE_MATCH_DOT;
 				break;
 			default:
-				if (!lastIsUpper && isUpper) {
+				// Check camelCase
+				if ("a" <= lastChar && lastChar <= "z" && "A" <= ch && ch <= "Z") {
 					matchBonuses[i] = SCORE_MATCH_CAPITAL;
 				} else {
 					matchBonuses[i] = 0;
@@ -82,64 +191,65 @@ function precomputeBonuses(haystack: string): BonusArray {
 		}
 
 		lastChar = ch;
-		lastIsUpper = isUpper;
 	}
 
 	return matchBonuses;
 }
 
-function _computeScore(
+function computeRow(
+	i: number,
 	n: number,
 	m: number,
-	needle: string,
-	haystack: string,
-): number {
-	const normalizedNeedle = needle.toLowerCase();
-	const normalizedHaystack = haystack.toLowerCase();
+	normalizedNeedle: string,
+	normalizedHaystack: string,
+	matchBonuses: BonusArray,
+	bestScores: ScoreArray,
+	bestMatchScores: ScoreArray,
+	lastBestScores: ScoreArray,
+	lastBestMatchScores: ScoreArray,
+) {
+	const gapScore = i === n - 1 ? SCORE_GAP_TRAILING : SCORE_GAP_INNER;
+	let prevScore = SCORE_MIN;
 
-	const matchBonuses = precomputeBonuses(haystack);
+	let prevBestScore = 0;
+	let prevBestMatchScore = 0;
 
-	const bestScores = new ScoreArray(m);
-	const bestMatchScores = new ScoreArray(m);
-
-	for (let i = 0; i < n; i++) {
-		const gapScore = i === n - 1 ? SCORE_GAP_TRAILING : SCORE_GAP_INNER;
-		let prevScore = SCORE_MIN;
-
-		let prevBestScore = 0;
-		let prevBestMatchScore = 0;
-
-		for (let j = 0; j < m; j++) {
-			if (normalizedNeedle[i] === normalizedHaystack[j]) {
-				let score = SCORE_MIN;
-				if (i === 0) {
-					score = j * SCORE_GAP_LEADING + matchBonuses[j] + SCORE_BASE;
-				} else if (j > 0) {
-					// i > 0 && j > 0
-					score = Math.max(
-						prevBestScore + matchBonuses[j],
-						// consecutive match, doesn't stack with match_bonus
-						prevBestMatchScore + SCORE_MATCH_CONSECUTIVE,
-					);
-				}
-				prevBestMatchScore = bestMatchScores[j];
-				bestMatchScores[j] = Math.min(
-					Math.max(SCORE_MIN, Math.trunc(score)),
-					SCORE_MAX,
+	for (let j = 0; j < m; j++) {
+		if (normalizedNeedle[i] === normalizedHaystack[j]) {
+			let score = SCORE_MIN;
+			if (i === 0) {
+				score = j * SCORE_GAP_LEADING + matchBonuses[j] + SCORE_BASE;
+			} else if (j > 0) {
+				// i > 0 && j > 0
+				score = Math.max(
+					prevBestScore + matchBonuses[j],
+					// consecutive match, doesn't stack with match_bonus
+					prevBestMatchScore + SCORE_MATCH_CONSECUTIVE,
 				);
-				prevScore = Math.max(score, prevScore + gapScore);
-			} else {
-				prevBestMatchScore = bestMatchScores[j];
-				bestMatchScores[j] = SCORE_MIN;
-				prevScore = prevScore + gapScore;
 			}
-			prevBestScore = bestScores[j];
-			bestScores[j] = Math.min(
-				Math.max(SCORE_MIN, Math.trunc(prevScore)),
+			prevBestMatchScore = lastBestMatchScores[j];
+			bestMatchScores[j] = Math.min(
+				Math.max(SCORE_MIN, Math.trunc(score)),
 				SCORE_MAX,
 			);
+			prevScore = Math.max(score, prevScore + gapScore);
+		} else {
+			prevBestMatchScore = lastBestMatchScores[j];
+			bestMatchScores[j] = SCORE_MIN;
+			prevScore = prevScore + gapScore;
 		}
+		prevBestScore = lastBestScores[j];
+		bestScores[j] = Math.min(
+			Math.max(SCORE_MIN, Math.trunc(prevScore)),
+			SCORE_MAX,
+		);
 	}
+}
 
-	return bestScores[m - 1];
+function createScoreMatrix(n: number, m: number): ScoreArray[] {
+	const arr = new Array(n);
+	for (let i = 0; i < n; i++) {
+		arr[i] = new ScoreArray(m);
+	}
+	return arr;
 }
